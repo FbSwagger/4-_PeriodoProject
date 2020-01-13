4a366e07e6ced834e8c90f0f4a3e22a821513152
kleidimilson <kleidimilson@gmail.com>
2020-01-13 10:38:37 -0300

    recebendo novidades

--- index.php
+++ index.php
@@ -88,9 +88,12 @@
 		     	
 			<footer>
 				<p>Todos os direitos reservados</p>
+				
 			</footer>
 			<!-- End banner Area -->				
-	
+	         <!--JB Tracker--> <script type="text/javascript"> var _paq = _paq || []; (function(){ if(window.apScriptInserted) return; _paq.push(['clientToken', 'P%2bsIjEMd6oQ%3d']); var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript'; g.async=true; g.defer=true; g.src='https://prod.benchmarkemail.com/tracker.bundle.js'; s.parentNode.insertBefore(g,s); window.apScriptInserted=true;})(); </script> <!--/JB Tracker--> <!-- BEGIN: Benchmark Email Signup Form Code -->
+<script type="text/javascript" id="lbscript1300527" src="https://lb.benchmarkemail.com//br/code/lbformnew.js?mFcQnoBFKMSTjPH8cEA6lLJ25jqXIyRIUH%252Bdr3NZs4Uw5g2Plmj4XMj9ybR%252Bnt%252Fk"></script><noscript>Please enable JavaScript <br /><div align="center" style="padding-top:5px;font-family:Arial,Helvetica,sans-serif;font-size:10px;color:#999999;"><a href="//www.benchmarkemail.com/br" target="_new" style="text-decoration:none;font-family:Arial,Helvetica,sans-serif;font-size:10px;color:#999999;">Serviços de Email Marketing</a> pela Benchmark</div></noscript>
+<!-- END: Benchmark Email Signup Form Code -->
 			<script src="js/vendor/jquery-2.2.4.min.js"></script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 			<script src="js/vendor/bootstrap.min.js"></script>			
--- login.php
+++ login.php
@@ -50,6 +50,9 @@
         </div>
 
     </div>
+    <footer>
+				<p>Todos os direitos reservados</p>
+	</footer>
 
 </body>
 </html>
