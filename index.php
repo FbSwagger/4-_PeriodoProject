b81eb24f815157f027f90f99ac71fba1afda20ce
kleidimilson <kleidimilson@gmail.com>
2020-01-13 10:43:24 -0300

    favicon

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 		<meta charset="UTF-8">
 		<!-- Site Title -->
 		<title>EVENTOPI</title>
+		<link rel="shortcut icon"  href="img/favicon.ico" />
 
 		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
 			<!--
